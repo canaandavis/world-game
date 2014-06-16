5cb6593fc1e7303799777603fd27f87055b225ec
Canaan Davis <canaandavis@Canaans-MacBook-Pro.local>
2014-06-16 09:16:24 -0500

    Add scenario controller and routes

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -1,7 +1,37 @@
 class ScenariosController < ApplicationController
   def show
+    @scenario = Scenario.find_by(params[:id])
+    render json: @scenario
   end
 
   def index
+    @scenarios = Scenario.all
+    @scenarios = @scenarios.map do |scenario|
+      {
+        id: scenario.id,
+        name: scenario.name
+      }
+    end
+    render json: @scenarios
+  end
+
+  def new
+    @scenario = Scenario.new
+  end
+
+  def create
+    scenario = Scenario.new(scenario_params)
+    if scenario.save
+      @message = "saved"
+    else
+      @message = "error"
+    end
+    redirect_to new_scenario_path
+  end
+
+  private
+
+  def scenario_params
+    params.require(:scenario).permit(:name, :weakness)
   end
 end
