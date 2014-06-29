b29ef7e3960d883048d58dbfdf9ff86e478088ec
Canaan Davis <canaandavis@Canaans-MacBook-Pro.local>
2014-06-29 14:21:54 -0500

    add asset url

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
 //= require angular.min
 //= require angular-route.min
 //= require angular-animate.min
