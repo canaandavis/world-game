9a0e358a4c2c2823d5f96052e5effa228249b7a3
Canaan Davis <canaandavis@Canaans-MacBook-Pro.local>
2014-06-29 12:24:17 -0500

    Update with static assets

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module WorldGame
     # config.i18n.default_locale = :de
     config.assets.enabled = true
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.serve_static_assets = true
   end
 end
